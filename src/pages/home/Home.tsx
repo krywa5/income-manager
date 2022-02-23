4eb850b47a17e84305711364f1c3071401d96922
Krystian Wasilewski <krystian.wasilewski@bytespack.com>
2022-02-23 20:20:30 +0100

    Init commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { Box, Typography } from "@mui/material";
+
+const Home: FunctionComponent = () => (
+  <Box sx={{ width: "100%" }}>
+    <Typography variant="h1" component="h1" align="center">
+      Hello world
+    </Typography>
+  </Box>
+);
+
+export default Home;
